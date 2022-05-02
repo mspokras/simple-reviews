c0e48703866623e1013b6a386f3d14270ec673c8
mspokras <mspokras@gmail.com>
2022-05-02 16:53:37 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Project idea
+https://www.youtube.com/watch?v=a_7Z7C_JCyo&list=WL&index=20
+
+CSS not mine as well
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Review from "./Review";
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our reviews</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+      <Review />
+    </main>
+  );
+}
+
+export default App;
